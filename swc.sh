790949d4d51737b68b3cc7143d8dc0f6f14155b3
aviv <141031195+avivyap@users.noreply.github.com>
2024-02-06 22:40:20 +0100

    Update swc.sh
    update men

--- swc.sh
+++ swc.sh
@@ -2,35 +2,31 @@
 
 # swc, Author avivyap (@avivyap)
 
-echo -ne  "\e[32m Hecho por\e[0m"
+#!/bin/bash
 
-echo -e "\e[33m Avivyap\e[0m"
-
+# Autor: avivyap (@avivyap)
+echo -e "\e[93mHecho por\e[0m"
+echo -e "\e[93mAvivyap\e[0m"
 sleep 0.5
-
-echo -ne "\e[32m Instagram\e[0m"
-
+echo -e "\e[93mInstagram\e[0m"
 sleep 0.5
-
-echo -e "\e[33m @avivyap\e[0m"
-
+echo -e "\e[93m@avivyap\e[0m"
 sleep 1
 
 # Definir colores
-yellowColour="\e[33m"
-greenColour="\e[32m"
-blueColour="\e[34m"
+yellowColour="\e[93m"
+purpleColour="\e[95m"
 endColour="\e[0m"
 
 # Listar todas las tarjetas de red disponibles numeradas y coloreadas
-echo -e "${greenColour}[+] ${blueColour}Tarjetas de red disponibles:${endColour}"
+echo -e "${purpleColour}[+] ${endColour}${yellowColour}Tarjetas de red disponibles:${endColour}"
 ip -o link show | awk -F': ' '{print NR".", "\033[1m"$2"\033[0m"}'
-echo -e "${greenColour}[-]${endColour}"
+echo -e "${purpleColour}[-]${endColour}"
 
 sleep 1
 
 # Preguntar por el número de la tarjeta de red a utilizar
-echo -e "${greenColour}[+]${endColour} Por favor ingresa el ${yellowColour}número${endColour} de la tarjeta de red que deseas configurar: "
+echo -e "${purpleColour}[+]${endColour} Por favor ingresa el ${yellowColour}número${endColour} de la tarjeta de red que deseas configurar: "
 read opcion
 
 # Obtener el nombre de la tarjeta de red seleccionada
@@ -42,7 +38,7 @@ ifconfig $nombre down
 sleep 2
 
 # Solicitar el nuevo nombre para la interfaz
-echo -e "${greenColour}[+]${endColour} ¿Qué ${yellowColour}nombre${endColour} le quieres poner a la tarjeta de red?"
+echo -e "${purpleColour}[+]${endColour} ¿Qué ${yellowColour}nombre${endColour} le quieres poner a la tarjeta de red?"
 read nuevo_nombre
 
 # Cambiar el nombre de la interfaz
@@ -58,4 +54,5 @@ sleep 2
 # Activar la interfaz con el nuevo nombre
 ifconfig $nuevo_nombre up
 
-echo -e "${greenColour}[+]${endColour} Configuración completada."
+echo -e "${purpleColour}[+]${endColour} Configuración completada."
+
